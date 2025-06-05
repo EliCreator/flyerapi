286b2dca1dbb1724d3ffde3562b45c7e1d7456e8
Eli <Eli.dev.tg@gmail.com>
2025-06-05 08:37:52 +0300

    1.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import codecs
 from setuptools import setup, find_packages
 
 
-VERSION = '1.2.1'
+VERSION = '1.2.2'
 DESCRIPTION = 'Asynchronous api of the Flyer service'
 
 here = os.path.abspath(os.path.dirname(__file__))
